44a4e4c309c05358b7d1d7a05e2b5c54af8498d2
小苟 <@DESKTOP-1A4QQBM>
2021-03-04 15:54:28 +0800

    实体

---
+++ src/DH.Inspection.Core/Line/StationDownInfo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DH.Inspection.Line
+{
+    /// <summary>
+    /// 站区下行表
+    /// </summary>
+    public class StationDownInfo
+    {
+        /// <summary>
+        /// 站区编号
+        /// </summary>
+        [MaxLength(4)]
+        [Required]
+        public int StationID { get; set; }
+
+        /// <summary>
+        /// 线路id
+        /// </summary>
+        [MaxLength(4)]
+        [Required]
+        public int LineID { get; set; }
+
+        public virtual LineInfo LineInfo { get; set; }
+
+        /// <summary>
+        /// 站区名称
+        /// </summary>
+        [MaxLength(128)]//最大长度
+        [Required]//必须填字段
+        public string StationName { get; set; }
+
+        /// <summary>
+        /// 起始千米
+        /// </summary>
+        [MaxLength(4)]
+        [Required]
+        public int StationKM { get; set; }
+
+        /// <summary>
+        /// 起始米
+        /// </summary>
+        [MaxLength(4)]
+        [Required]
+        public int StationM { get; set; }
+        /// <summary>
+        /// 结束千米
+        /// </summary>
+        [MaxLength(4)]
+        [Required]
+        public int EndKM { get; set; }
+
+        /// <summary>
+        /// 结束米
+        /// </summary>
+        [MaxLength(4)]
+        [Required]
+        public int EndM { get; set; }
+    }
+}
